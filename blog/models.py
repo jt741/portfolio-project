fb6656f000a729de1cc24bd4ae5fb3a00c334199
Jessye Tu <jt741@cam.ac.uk>
2020-04-07 20:32:24 +0100

    sort of getting the blog to work

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,20 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
     feeling_good = models.BooleanField(default=True)
+
+#to help with admin viewing 
+    def __str__(self):
+        '''
+        Its one of those magic functions!
+        This is how to show the name up in the admin page
+        '''
+        return self.title
+
+
+#to add polish create functions within your model class that do specifics
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
